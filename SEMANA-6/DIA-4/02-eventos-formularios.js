8956eb7b9f0e1a2c8bc83f117880d2f1b7808749
Luisgfv1 <Luisgfv1@gmail.com>
2020-07-30 22:11:11 -0500

    Versión 30/07

---
+++ SEMANA-6/DIA-4/02-eventos-formularios.js
@@ -0,0 +1,12 @@
+/**
+ * Elemento.onload
+ * función cuando se dispara cuándo se han cargado todos los elementos hijos de un
+ * elemento
+ */
+window.onload = () => {
+  let formulario = document.getElementById("formulario");
+  let inputEmail = document.getElementById("inputEmail");
+  let inputPassword = document.getElementById("inputPassword");
+  let enlace = document.getElementById("enlace");
+  console.log(formulario);
+};
